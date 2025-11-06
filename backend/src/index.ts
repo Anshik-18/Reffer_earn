0b2745fea79c6ba770fd934508e2e0e5343fa5f3
Anshik jain <anshikjain56@gmail.com>
2025-11-06 09:30:55 +0530

    bug fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,23 +30,13 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    preflightContinue:false
   })
 );
 app.use(express.json());
 
 //  DB CONNECT 
-app.options("/*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  res.sendStatus(200);
-});
+
 mongoose
   .connect(process.env.MONGO_URI as string)
   .then(() => console.log(" MongoDB Connected"))
